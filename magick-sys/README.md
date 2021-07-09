32322fc32845f7ac2e661bb899f3b16c6e0fb9ff
Petru Szemereczki <petru.szemereczki@tazz.ro>
2021-07-09 13:29:14 +0300

    - more work into making of magick-sys crate

---
+++ magick-sys/README.md
@@ -0,0 +1,10 @@
+# magick-sys
+
+this is a sys crate for imagemagick.
+
+it's unstable / wip, so don't use it unless you're working on it.
+
+the main idea of forking and working on this project is:
+- the issue is more than a year old on the main repo
+- nobody worked at it since then
+- in our company, we need to statically compile imagemagick, which isn't working by default on original repo
--- magick-sys/build.rs
+++ magick-sys/build.rs
@@ -1,6 +1,11 @@
+use std::collections::HashSet;
 use std::env;
-use std::path::Path;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::{Path, PathBuf};
 use std::process::{exit, Command};
+
+static HEADER: &'static str = "#include <MagickWand/MagickWand.h>\n";
 
 fn main() -> Result<(), Error> {
     if cfg!(not(target_os = "linux")) {
@@ -27,30 +32,29 @@ fn build_imagemagick() -> Result<(), Error> {
 
     configure_cmd
         .current_dir(&magick_cfg.path())
-        .arg("--disable-osx-universal-binary")
+        // .arg("--disable-osx-universal-binary")
         .arg("--with-magick-plus-plus=no")
         .arg("--with-perl=no")
-        .arg("--disable-dependency-tracking")
-        .arg("--disable-silent-rules")
-        .arg("--disable-opencl")
-        .arg("--with-freetype=yes")
-        .arg("--with-modules")
-        .arg("--with-openjp2")
-        .arg("--with-openexr")
-        .arg("--with-webp=yes")
-        .arg("--with-heic=no")
-        .arg("--with-gslib")
-        .arg("--without-fftw")
-        .arg("--without-pango")
-        .arg("--without-x")
-        .arg("--without-wmf")
-        .arg("--prefix");
+        // .arg("--disable-dependency-tracking")
+        // .arg("--disable-silent-rules")
+        // .arg("--disable-opencl");
+        // .arg("--with-freetype=yes")
+        .arg("--with-modules");
+    // .arg("--with-openjp2")
+    // .arg("--with-openexr")
+    // .arg("--with-webp=yes")
+    // .arg("--with-heic=no")
+    // .arg("--with-gslib")
+    // .arg("--without-fftw")
+    // .arg("--without-pango")
+    // .arg("--without-x")
+    // .arg("--without-wmf");
 
     if cfg!(feature = "static") {
         configure_cmd.arg("--disable-shared").arg("--enable-static");
     }
 
-    configure_cmd.arg(&out_dir);
+    configure_cmd.arg("--prefix").arg(&out_dir);
 
     match configure_cmd.output() {
         Ok(out) => {
@@ -95,7 +99,7 @@ fn build_imagemagick() -> Result<(), Error> {
     }
     eprintln!("finished `make install`");
 
-    let previous_value = env::var("PKG_CONFIG_PATH").unwrap();
+    let previous_value = env::var("PKG_CONFIG_PATH");
 
     env::set_var("PKG_CONFIG_PATH", format!("{}/lib/pkgconfig", &out_dir));
     let lib = pkg_config::Config::new()
@@ -104,10 +108,94 @@ fn build_imagemagick() -> Result<(), Error> {
         .probe("MagickWand")?;
 
     // restore env
-    env::set_var("PKG_CONFIG_PATH", previous_value);
+    if let Ok(previous_value) = previous_value {
+        env::set_var("PKG_CONFIG_PATH", previous_value);
+    }
 
-    for d in lib.include_paths {
+    for lib in &lib.libs {
+        let kind = if cfg!(feature = "static") {
+            "static"
+        } else {
+            "dylib"
+        };
+        println!("cargo:rustc-link-lib={}={}", kind, lib);
+    }
+
+    for d in &lib.include_paths {
         println!("cargo:include={}", d.to_string_lossy());
+    }
+
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let bindings_path_str = out_dir.join("bindings.rs");
+
+    #[derive(Debug)]
+    struct IgnoreMacros(HashSet<String>);
+
+    impl bindgen::callbacks::ParseCallbacks for IgnoreMacros {
+        fn will_parse_macro(&self, name: &str) -> bindgen::callbacks::MacroParsingBehavior {
+            if self.0.contains(name) {
+                bindgen::callbacks::MacroParsingBehavior::Ignore
+            } else {
+                bindgen::callbacks::MacroParsingBehavior::Default
+            }
+        }
+    }
+
+    let ignored_macros = IgnoreMacros(
+        vec![
+            "FP_INFINITE".into(),
+            "FP_NAN".into(),
+            "FP_NORMAL".into(),
+            "FP_SUBNORMAL".into(),
+            "FP_ZERO".into(),
+            "IPPORT_RESERVED".into(),
+            "FP_INT_UPWARD".into(),
+            "FP_INT_DOWNWARD".into(),
+            "FP_INT_TOWARDZERO".into(),
+            "FP_INT_TONEARESTFROMZERO".into(),
+            "FP_INT_TONEAREST".into(),
+        ]
+        .into_iter()
+        .collect(),
+    );
+
+    if !Path::new(&bindings_path_str).exists() {
+        // Create the header file that rust-bindgen needs as input.
+        let gen_h_path = out_dir.join("gen.h");
+        let mut gen_h = File::create(&gen_h_path).expect("could not create file");
+        gen_h
+            .write_all(HEADER.as_bytes())
+            .expect("could not write header file");
+
+        // Geneate the bindings.
+        let mut builder = bindgen::Builder::default()
+            .emit_builtins()
+            .ctypes_prefix("libc")
+            .raw_line("extern crate libc;")
+            .header(gen_h_path.to_str().unwrap())
+            .size_t_is_usize(true)
+            .parse_callbacks(Box::new(ignored_macros))
+            .blocklist_type("timex")
+            .blocklist_function("clock_adjtime");
+
+        for d in &lib.include_paths {
+            builder = builder.clang_arg(format!("-I{}", d.to_string_lossy()));
+        }
+
+        let bindings = builder.generate().unwrap();
+        // let mut file = File::create(&bindings_path_str).expect("could not create bindings file");
+        // Work around the include! issue in rustc (as described in the
+        // rust-bindgen README file) by wrapping the generated code in a
+        // `pub mod` declaration; see issue #359 in (old) rust-bindgen.
+        // file.write(b"pub mod bindings {\n").unwrap();
+        // file.write(bindings.to_string().as_bytes()).unwrap();
+        // file.write(b"\n}").unwrap();
+
+        bindings
+            .write_to_file(out_dir.join("bindings.rs"))
+            .expect("Couldn't write bindings!");
+
+        std::fs::remove_file(&gen_h_path).expect("could not remove header file");
     }
 
     Ok(())
